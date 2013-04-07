45909266c94fb6e84ad8610bd0619867a3fe8b89
ikoen <ilias.koen@dukodestudio.com>
2013-04-07 14:02:26 -0400

    parse arguments

--- openir_atmosphericcorrection.py
+++ openir_atmosphericcorrection.py
@@ -19,6 +19,62 @@ import sys
 import os.path
 
 def Usage():
-    print("""
-openir_atmosphericcorrection.py TO DO """)
+    print(""" openir_atmosphericcorrection.py [sourcefile] [dstfile] """)
     sys.exit(1)
+
+# =============================================================================
+# 	Mainline
+# =============================================================================
+
+quiet_flag = 0
+src_filename = None
+src_band = 1
+
+dst_filename = None
+format = 'GTiff'
+creation_options = []
+
+argv = gdal.GeneralCmdLineProcessor( sys.argv )
+argv = gdal.GeneralCmdLineProcessor( sys.argv )
+if argv is None:
+    sys.exit( 0 )
+
+print "x"
+# ############################
+# Parse command line arguments.
+# ############################
+i = 1
+while i < len(argv):
+    arg = argv[i]
+
+    if arg[:2] == '-h':
+        Usage()
+
+    elif src_filename is None:
+        src_filename = argv[i]
+
+    elif dst_filename is None:
+        dst_filename = argv[i]
+
+    else:
+        Usage()
+
+    i = i + 1
+
+if src_filename is None:
+    Usage()
+
+
+print "SOURCE FILE "+ src_filename
+print "DESTINATION FILE "+ dst_filename
+# ###################################################
+# DETERMINE THE BAND. 
+# ###################################################
+
+
+
+
+
+#
+# PROCESS TILE
+#
