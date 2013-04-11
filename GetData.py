3d2be71e5b74bdc7ae58d70c2526837d69e33753
Wendy Cheang <wcheang@mit.edu>
2013-04-11 16:20:52 -0400

    added getbranch function

--- GetData.py
+++ GetData.py
@@ -1,3 +1,4 @@
+import re
 import gdal
 from gdalconst import *
 
@@ -16,4 +17,12 @@ def getData(filename):
         if not geotransform is None:
             print 'Origin = (',geotransform[0], ',',geotransform[3],')'
             print 'Pixel Size = (',geotransform[1], ',',geotransform[5],')'
+    ##Getting the bands
+    
+def getBand(filename):
+    band = re.search('B\\d',filename).group(0)
+    print band
+    band = re.search('\\d',band).group(0)
+    return int(band)
+    
     
