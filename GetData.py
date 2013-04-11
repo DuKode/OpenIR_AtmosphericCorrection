38f1e2997cee0e8cff2102abb0af38c2bcc56ce6
Wendy Cheang <wcheang@mit.edu>
2013-04-11 19:42:39 -0400

    added a getpixeldata function that reads a block of 100 x  150. Note that this is probably the best way to read data in python as anything else would not work. Also suspiciously everything reports a zero but I haven't figured it out if it's just my code is buggy or if that block is empty. Are there smaller files we can test on?

--- GetData.py
+++ GetData.py
@@ -1,11 +1,14 @@
 import re
 import gdal
+import struct
+import sys
 from gdalconst import *
 
 def getData(filename):
     dataset = gdal.Open( filename, GA_ReadOnly )
     if dataset is None:
-        return "Could not read file."
+        print "Could not read file."
+        return None
     else:
         print 'Driver: ', dataset.GetDriver().ShortName,'/', \
           dataset.GetDriver().LongName
@@ -14,15 +17,45 @@ def getData(filename):
         print 'Projection is ',dataset.GetProjection()
     
         geotransform = dataset.GetGeoTransform()
-        if not geotransform is None:
-            print 'Origin = (',geotransform[0], ',',geotransform[3],')'
-            print 'Pixel Size = (',geotransform[1], ',',geotransform[5],')'
-    ##Getting the bands
+    if geotransform is None:
+        print "There is something wrong with the metadata"
+        return None
+    else:
+        print 'Origin = (',geotransform[0], ',',geotransform[3],')'
+        print 'Pixel Size = (',geotransform[1], ',',geotransform[5],')'
+
+def getPixelData(filename):
+    dataset = gdal.Open(filename)
+    if dataset is None:
+        print "Could not read file."
+        return None
+    bandnumber = getBand(filename)
+    if bandnumber == None:
+        print "Could not get band"
+        return None
+    band = dataset.GetRasterBand(bandnumber)
+    pixelarray = band.ReadAsArray(0,0,100,150)
+    for row in pixelarray:
+        for pixel in row:
+            sys.stdout.write(pixel)
+        print(" ")
+    dataset = None
+    ##scanline = band.ReadRaster(0,0, band.GetMinimum(),1,band.GetXSize(),1,GDT_FLOAT32)
+    ##data = struct.unpack('f' * band.Xsize,scanline)
+    ##print data
     
+    
+    
+    
+##Returns the values of the band as an int. If something has failed it returns a null value.
+
 def getBand(filename):
     band = re.search('B\\d',filename).group(0)
-    print band
     band = re.search('\\d',band).group(0)
+    if band == None:
+        return None
     return int(band)
-    
+
+
+
     
