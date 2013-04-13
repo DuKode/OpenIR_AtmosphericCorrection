204fa633fabfb7f0dde5454c32bd16bb60dfd21f
Wendy Cheang <wcheang@mit.edu>
2013-04-13 17:56:18 -0400

    I have code that copies a band over to another file. It's not quite what we want because the image doesn't appear which means that there is something I fundamentally don't understand about gdal. The band copy code works and is confirmed to work. Also I fixed up some stuff like exiting if we can't read files properly

--- GetData.py
+++ GetData.py
@@ -2,13 +2,14 @@ import re
 import gdal
 import struct
 import sys
+import numpy
 from gdalconst import *
 
 def getData(filename):
     dataset = gdal.Open( filename, GA_ReadOnly )
     if dataset is None:
         print "Could not read file."
-        return None
+        sys.exit(1)
     else:
         print 'Driver: ', dataset.GetDriver().ShortName,'/', \
           dataset.GetDriver().LongName
@@ -24,27 +25,50 @@ def getData(filename):
         print 'Origin = (',geotransform[0], ',',geotransform[3],')'
         print 'Pixel Size = (',geotransform[1], ',',geotransform[5],')'
 
-def getPixelData(filename):
-    dataset = gdal.Open(filename)
-    if dataset is None:
-        print "Could not read file."
-        return None
-    bandnumber = getBand(filename)
-    if bandnumber == None:
-        print "Could not get band"
-        return None
-    band = dataset.GetRasterBand(bandnumber)
-    pixelarray = band.ReadAsArray(0,0,100,150)
-    for row in pixelarray:
-        for pixel in row:
-            sys.stdout.write(pixel)
-        print(" ")
-    dataset = None
-    ##scanline = band.ReadRaster(0,0, band.GetMinimum(),1,band.GetXSize(),1,GDT_FLOAT32)
-    ##data = struct.unpack('f' * band.Xsize,scanline)
-    ##print data
+#fetchs the pixel data of the file. Does it in a for loop and one line at a time
+#filename is the name of the file. must have a B followed by a number stating the band
+
+def changePixelData(filename,amount):
     
+    inDS = gdal.Open(filename)
+    bandnumber = getBand(filename)       
+    band = inDS.GetRasterBand(bandnumber)
+    driver = inDS.GetDriver()
     
+    driver.Create("cpy" + filename, inDS.RasterXSize, inDS.RasterYSize,bandnumber, GDT_UInt16)
+    outDS = gdal.Open("cpy" + filename)
+    outBand = outDS.GetRasterBand(bandnumber)
+    outdata = [[]]
+    data = [[]]
+
+    i = 0
+    while i <= xrange(inDS.RasterYSize):
+        rowsize = 10
+        if rowsize+i > inDS.RasterYSize:
+            rowsize = inDS.RasterYSize - i - 1
+            if rowsize == 0:
+                break
+        data = band.ReadAsArray(0,i,inDS.RasterXSize,10)
+        outBand.WriteArray(data,0,i)
+        i = i + 10
+	
+
+    outdata = outBand.ReadAsArray(0,0,inDS.RasterXSize, 3)
+    for row in xrange(len(outdata)):
+        for col in xrange(len(outdata)):
+            if not data[row][col] == outdata[row][col]:
+                print row
+                print col
+                print data[row][col]
+                print outdata[row][col]
+                return False
+        
+    outDS.SetProjection(inDS.GetProjection())
+    outDS.SetGeoTransform(inDS.GetGeoTransform())
+            
+    return True
+
+
     
     
 ##Returns the values of the band as an int. If something has failed it returns a null value.
@@ -53,5 +77,9 @@ def getBand(filename):
     band = re.search('B\\d',filename).group(0)
     band = re.search('\\d',band).group(0)
     if band == None:
-        return None
+        sys.exit(1)
     return int(band)
+
+
+
+    
